b5ba97bc530eb11f75347aeba7b6b7ccf157c446
eylemdogan <eylemdogan603@gmail.com>
2025-08-14 17:55:10 +0300

    Update tailwind.config.js with new configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#b69cff",
+        background: "#181624"
+      }
+    }
+  },
+  plugins: []
+};
